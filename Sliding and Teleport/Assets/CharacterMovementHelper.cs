6860e56bc771bdc8f82470f338d8073284a7e6b6
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-12-16 01:33:39 +0700

    character movement

---
+++ Sliding and Teleport/Assets/CharacterMovementHelper.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CharacterMovementHelper : MonoBehaviour
+{
+    private XROrigin xROrigin;
+    private CharacterController characterController;
+    private CharacterController driver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xROrigin = GetComponent<XROrigin>();
+        characterController = GetComponent<CharacterController>();
+        driver = GetComponent<CharacterControllerDriver>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCharacterController();
+    }
+
+        /// <summary>
+        /// Updates the <see cref="CharacterController.height"/> and <see cref="CharacterController.center"/>
+        /// based on the camera's position.
+        /// </summary>
+        protected virtual void UpdateCharacterController()
+        {
+            if (XROrigin == null || CharacterController == null)
+                return;
+
+            var height = Mathf.Clamp(XROrigin.CameraInOriginSpaceHeight, driver.MinHeight, driver.MaxHeight);
+
+            Vector3 center = XROrigin.CameraInOriginSpacePos;
+            center.y = height / 2f + CharacterController.skinWidth;
+
+            CharacterController.height = height;
+            CharacterController.center = center;
+        }
+}
