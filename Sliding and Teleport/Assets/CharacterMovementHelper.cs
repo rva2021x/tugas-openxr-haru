0dd3be64e7c73c7b817658c5987c6b0217abbc69
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-12-17 01:07:29 +0700

    create new project, import assets, add vuforia package

--- Sliding and Teleport/Assets/CharacterMovementHelper.cs
+++ Sliding and Teleport/Assets/CharacterMovementHelper.cs
@@ -5,39 +5,39 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class CharacterMovementHelper : MonoBehaviour
 {
-    private XROrigin xROrigin;
-    private CharacterController characterController;
-    private CharacterController driver;
+    // private XROrigin xROrigin;
+    // private CharacterController characterController;
+    // private CharacterController driver;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        xROrigin = GetComponent<XROrigin>();
-        characterController = GetComponent<CharacterController>();
-        driver = GetComponent<CharacterControllerDriver>();
-    }
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     xROrigin = GetComponent<XROrigin>();
+    //     characterController = GetComponent<CharacterController>();
+    //     driver = GetComponent<CharacterControllerDriver>();
+    // }
 
-    // Update is called once per frame
-    void Update()
-    {
-        UpdateCharacterController();
-    }
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     UpdateCharacterController();
+    // }
 
         /// <summary>
         /// Updates the <see cref="CharacterController.height"/> and <see cref="CharacterController.center"/>
         /// based on the camera's position.
         /// </summary>
-        protected virtual void UpdateCharacterController()
-        {
-            if (XROrigin == null || CharacterController == null)
-                return;
+        // protected virtual void UpdateCharacterController()
+        // {
+        //     if (XROrigin == null || CharacterController == null)
+        //         return;
 
-            var height = Mathf.Clamp(XROrigin.CameraInOriginSpaceHeight, driver.MinHeight, driver.MaxHeight);
+        //     var height = Mathf.Clamp(XROrigin.CameraInOriginSpaceHeight, driver.MinHeight, driver.MaxHeight);
 
-            Vector3 center = XROrigin.CameraInOriginSpacePos;
-            center.y = height / 2f + CharacterController.skinWidth;
+        //     Vector3 center = XROrigin.CameraInOriginSpacePos;
+        //     center.y = height / 2f + CharacterController.skinWidth;
 
-            CharacterController.height = height;
-            CharacterController.center = center;
-        }
+        //     CharacterController.height = height;
+        //     CharacterController.center = center;
+        // }
 }
